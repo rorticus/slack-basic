4e2e9983a4b44de340c37f04c6f0d6c8f0608a7b
rorticus <rorticus@gmail.com>
2021-09-01 18:56:49 +0000

    fixing repl

--- src/cli/repl.ts
+++ src/cli/repl.ts
@@ -3,42 +3,57 @@ import { Statement } from '../basic/ast';
 import { Parser } from '../basic/parser';
 import Lexer from '../basic/lexer';
 import { isError } from '../basic/object';
+import * as readLineSync from 'readline-sync';
+
+const context = new Context({
+    print(str: string): Promise<void> {
+        console.log(str);
+        return Promise.resolve();
+    },
+    input(message?: string): Promise<string> {
+        return Promise.resolve('');
+    },
+    save(filename: string, statements: Statement[]): Promise<void> {
+        return Promise.reject('not implemented');
+    },
+    load(filename: string): Promise<Statement[]> {
+        return Promise.reject('not implemented');
+    },
+    createImage(width: number, height: number): Promise<BasicCanvas> {
+        return Promise.reject('not implemented');
+    },
+});
 
 const run = async (code: string) => {
-    const context = new Context({
-        print(str: string): Promise<void> {
-            console.log(str);
-            return Promise.resolve();
-        },
-        input(message?: string): Promise<string> {
-            return Promise.resolve('');
-        },
-        save(filename: string, statements: Statement[]): Promise<void> {
-            return Promise.reject('not implemented');
-        },
-        load(filename: string): Promise<Statement[]> {
-            return Promise.reject('not implemented');
-        },
-        createImage(width: number, height: number): Promise<BasicCanvas> {
-            return Promise.reject('not implemented');
-        },
-    });
+    const parser = new Parser(new Lexer(code.trim()));
+    const statement = parser.parseStatement();
 
-    const lines = code.split('\n');
-    for (let i = 0; i < lines.length; i++) {
-        const parser = new Parser(new Lexer(lines[i].trim()));
-        const statement = parser.parseStatement();
-
-        if (statement) {
-            const result = await context.runImmediateStatement(statement);
-            if (isError(result)) {
-                console.log(result.toString());
-            }
+    if (statement) {
+        const result = await context.runImmediateStatement(statement);
+        if (isError(result)) {
+            console.log(result.toString());
         }
+
+        return result;
     }
+
+    return null;
 };
 
-run(`
-10 PRINT "hello" : GOTO 10
-RUN
-`);
+async function main() {
+    while (1) {
+        const line = readLineSync.question('> ');
+        if (line === 'exit') {
+            return;
+        }
+
+        const result = await run(line);
+
+        
+        if (result) {
+            console.log(result.toString());
+        }
+    }
+}
+
+main();
